c90e74f5800d93cbc04dc97fdec53dd9b7c98e5a
İsmail Üçel <ismailucel@gmail.com>
2023-02-03 13:58:15 +0300

    initial commit
    Project created

---
+++ Optik Arayüz/Optik Arayüz UI/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Optik_Arayüz_UI.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
