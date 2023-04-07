e857eaf2e43203983f47cf6865ec08481f1a9408
İsmail Üçel <ismailucel@gmail.com>
2023-04-07 17:22:11 +0300

    initialMigration added

---
+++ Optik Arayüz/Optik Arayüz UI/Data/Migrations/20230407140646_initialMigration.cs
@@ -0,0 +1,301 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Optik_Arayüz_UI.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class initialMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Choices",
+                columns: table => new
+                {
+                    ChoiceId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ChoiceCount = table.Column<int>(type: "int", nullable: false),
+                    XLength = table.Column<double>(type: "float", nullable: false),
+                    YLength = table.Column<double>(type: "float", nullable: false),
+                    Label = table.Column<double>(type: "float", nullable: false),
+                    ChoiceId1 = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Choices", x => x.ChoiceId);
+                    table.ForeignKey(
+                        name: "FK_Choices_Choices_ChoiceId1",
+                        column: x => x.ChoiceId1,
+                        principalTable: "Choices",
+                        principalColumn: "ChoiceId");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ExamPapers",
+                columns: table => new
+                {
+                    ExamPaperId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ExamPaperName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ExamPaperTitle = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ExamPapers", x => x.ExamPaperId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Faculties",
+                columns: table => new
+                {
+                    FacultyId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FacultyName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    FacultyAddress = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    FacultyMail = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    FacultyPhoneNumber = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Faculties", x => x.FacultyId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Grades",
+                columns: table => new
+                {
+                    GradeId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    XLength = table.Column<double>(type: "float", nullable: false),
+                    YLength = table.Column<double>(type: "float", nullable: false),
+                    QuestionCount = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Grades", x => x.GradeId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Logos",
+                columns: table => new
+                {
+                    LogoId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ImagePath = table.Column<int>(type: "int", nullable: false),
+                    XLength = table.Column<double>(type: "float", nullable: false),
+                    YLength = table.Column<double>(type: "float", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Logos", x => x.LogoId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Numbers",
+                columns: table => new
+                {
+                    NumberId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    XLength = table.Column<double>(type: "float", nullable: false),
+                    YLength = table.Column<double>(type: "float", nullable: false),
+                    Length = table.Column<double>(type: "float", nullable: false),
+                    Label = table.Column<double>(type: "float", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Numbers", x => x.NumberId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Students",
+                columns: table => new
+                {
+                    StudentId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FirstName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    LastName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CourseName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    XLength = table.Column<double>(type: "float", nullable: false),
+                    YLength = table.Column<double>(type: "float", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Students", x => x.StudentId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Tests",
+                columns: table => new
+                {
+                    TestId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    QuestionCount = table.Column<int>(type: "int", nullable: false),
+                    XLength = table.Column<double>(type: "float", nullable: false),
+                    YLength = table.Column<double>(type: "float", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tests", x => x.TestId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Texts",
+                columns: table => new
+                {
+                    TextId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    TextContent = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    FontSize = table.Column<int>(type: "int", nullable: false),
+                    FontType = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Texts", x => x.TextId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ExamPaperElements",
+                columns: table => new
+                {
+                    ExamPaperElementId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Type = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    X = table.Column<double>(type: "float", nullable: false),
+                    Y = table.Column<double>(type: "float", nullable: false),
+                    ExamPaperId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ExamPaperElements", x => x.ExamPaperElementId);
+                    table.ForeignKey(
+                        name: "FK_ExamPaperElements_ExamPapers_ExamPaperId",
+                        column: x => x.ExamPaperId,
+                        principalTable: "ExamPapers",
+                        principalColumn: "ExamPaperId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    UserId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FirstName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    LastName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Password = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Address = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    PhoneNumber = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Role = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    FacultyId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.UserId);
+                    table.ForeignKey(
+                        name: "FK_Users_Faculties_FacultyId",
+                        column: x => x.FacultyId,
+                        principalTable: "Faculties",
+                        principalColumn: "FacultyId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Exams",
+                columns: table => new
+                {
+                    ExamId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ExamName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    TestCount = table.Column<int>(type: "int", nullable: false),
+                    ClassicCount = table.Column<int>(type: "int", nullable: false),
+                    ExamPaperId = table.Column<int>(type: "int", nullable: false),
+                    UserId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Exams", x => x.ExamId);
+                    table.ForeignKey(
+                        name: "FK_Exams_ExamPapers_ExamPaperId",
+                        column: x => x.ExamPaperId,
+                        principalTable: "ExamPapers",
+                        principalColumn: "ExamPaperId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Exams_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "UserId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Choices_ChoiceId1",
+                table: "Choices",
+                column: "ChoiceId1");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ExamPaperElements_ExamPaperId",
+                table: "ExamPaperElements",
+                column: "ExamPaperId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Exams_ExamPaperId",
+                table: "Exams",
+                column: "ExamPaperId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Exams_UserId",
+                table: "Exams",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_FacultyId",
+                table: "Users",
+                column: "FacultyId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Choices");
+
+            migrationBuilder.DropTable(
+                name: "ExamPaperElements");
+
+            migrationBuilder.DropTable(
+                name: "Exams");
+
+            migrationBuilder.DropTable(
+                name: "Grades");
+
+            migrationBuilder.DropTable(
+                name: "Logos");
+
+            migrationBuilder.DropTable(
+                name: "Numbers");
+
+            migrationBuilder.DropTable(
+                name: "Students");
+
+            migrationBuilder.DropTable(
+                name: "Tests");
+
+            migrationBuilder.DropTable(
+                name: "Texts");
+
+            migrationBuilder.DropTable(
+                name: "ExamPapers");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+
+            migrationBuilder.DropTable(
+                name: "Faculties");
+        }
+    }
+}
