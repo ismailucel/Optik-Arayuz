5e5d5159b873f8cec5f7cee9cf7b3ff577595398
İsmail Üçel <ismailucel@gmail.com>
2023-05-03 22:06:07 +0300

    Help page added

---
+++ Optik Arayüz/Optik Arayüz UI/Controllers/HelpController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Optik_Arayüz_UI.Controllers
+{
+    public class HelpController : Controller
+    {
+        private readonly ILogger<HelpController> _logger;
+
+        public HelpController(ILogger<HelpController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
