9af4f5cd416243fb4dba43e281c31658ccdfedbb
İsmail Üçel <ismailucel@gmail.com>
2023-05-04 17:52:01 +0300

    exam model updated

--- Optik Arayuz/Optik Arayuz UI/Models/Exam.cs
+++ Optik Arayuz/Optik Arayuz UI/Models/Exam.cs
@@ -14,7 +14,7 @@ namespace Optik_Arayuz_UI.Models
         public int ExamPaperId { get; set; }
         public ExamPaper? ExamPaper { get; set; }
         [ForeignKey("User")]
-        public int UserId { get; set; }
+        public string UserId { get; set; }
         public virtual User? User { get; set; }
     }
 }
