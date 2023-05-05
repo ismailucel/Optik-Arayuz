727eac3029507567334b7def9282433ac98568db
İsmail Üçel <ismailucel@gmail.com>
2023-05-05 16:31:30 +0300

    migration

---
+++ Optik Arayuz/Optik Arayuz UI/Data/Migrations/20230505085431_initializer.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Optik_Arayüz_UI.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class initializer : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_AspNetUsers_Faculties_FacultyId",
+                table: "AspNetUsers");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AspNetUsers_Faculties_FacultyId",
+                table: "AspNetUsers",
+                column: "FacultyId",
+                principalTable: "Faculties",
+                principalColumn: "FacultyId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_AspNetUsers_Faculties_FacultyId",
+                table: "AspNetUsers");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AspNetUsers_Faculties_FacultyId",
+                table: "AspNetUsers",
+                column: "FacultyId",
+                principalTable: "Faculties",
+                principalColumn: "FacultyId",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
