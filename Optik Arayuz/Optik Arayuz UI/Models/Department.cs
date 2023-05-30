aec2a113c40f71d2f6a968919bd0d01a7ea8eab4
İsmail Üçel <ismailucel@gmail.com>
2023-05-30 22:18:21 +0300

    department model added

---
+++ Optik Arayuz/Optik Arayuz UI/Models/Department.cs
@@ -0,0 +1,20 @@
+﻿using Optik_Arayuz_UI.Models;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Optik_Arayüz_UI.Models
+{
+    public class Department
+    {
+        [Key]
+        public int DepartmentId { get; set; }
+        public string? DepartmentName { get; set; }
+        public string? DepartmentAddress { get; set; }
+        public string? DepartmentMail { get; set; }
+        public string? DepartmentPhoneNumber { get; set; }
+
+        [ForeignKey("Faculty")]
+        public int? FacultyId { get; set; }
+        public virtual Faculty? Faculty { get; set; }
+    }
+}
