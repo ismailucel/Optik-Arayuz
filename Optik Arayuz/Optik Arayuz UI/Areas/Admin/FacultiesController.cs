de3b5e0d877637589e4b4bf2d43a5555d219908f
İsmail Üçel <ismailucel@gmail.com>
2023-05-30 15:17:03 +0300

    faculties controller and views added

---
+++ Optik Arayuz/Optik Arayuz UI/Areas/Admin/FacultiesController.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Optik_Arayuz_UI.Data;
+using Optik_Arayuz_UI.Models;
+
+namespace Optik_Arayuz_UI.Areas.Admin
+{
+    [Area("Admin")]
+    public class FacultiesController : Controller
+    {
+        private readonly OptikArayuzDbContext _context;
+
+        public FacultiesController(OptikArayuzDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Admin/Faculties
+        public async Task<IActionResult> Index()
+        {
+              return _context.Faculties != null ? 
+                          View(await _context.Faculties.ToListAsync()) :
+                          Problem("Entity set 'OptikArayuzDbContext.Faculties'  is null.");
+        }
+
+        // GET: Admin/Faculties/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Faculties == null)
+            {
+                return NotFound();
+            }
+
+            var faculty = await _context.Faculties
+                .FirstOrDefaultAsync(m => m.FacultyId == id);
+            if (faculty == null)
+            {
+                return NotFound();
+            }
+
+            return View(faculty);
+        }
+
+        // GET: Admin/Faculties/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Admin/Faculties/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("FacultyId,FacultyName,FacultyAddress,FacultyMail,FacultyPhoneNumber")] Faculty faculty)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(faculty);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(faculty);
+        }
+
+        // GET: Admin/Faculties/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Faculties == null)
+            {
+                return NotFound();
+            }
+
+            var faculty = await _context.Faculties.FindAsync(id);
+            if (faculty == null)
+            {
+                return NotFound();
+            }
+            return View(faculty);
+        }
+
+        // POST: Admin/Faculties/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("FacultyId,FacultyName,FacultyAddress,FacultyMail,FacultyPhoneNumber")] Faculty faculty)
+        {
+            if (id != faculty.FacultyId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(faculty);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!FacultyExists(faculty.FacultyId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(faculty);
+        }
+
+        // GET: Admin/Faculties/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Faculties == null)
+            {
+                return NotFound();
+            }
+
+            var faculty = await _context.Faculties
+                .FirstOrDefaultAsync(m => m.FacultyId == id);
+            if (faculty == null)
+            {
+                return NotFound();
+            }
+
+            return View(faculty);
+        }
+
+        // POST: Admin/Faculties/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Faculties == null)
+            {
+                return Problem("Entity set 'OptikArayuzDbContext.Faculties'  is null.");
+            }
+            var faculty = await _context.Faculties.FindAsync(id);
+            if (faculty != null)
+            {
+                _context.Faculties.Remove(faculty);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool FacultyExists(int id)
+        {
+          return (_context.Faculties?.Any(e => e.FacultyId == id)).GetValueOrDefault();
+        }
+    }
+}
