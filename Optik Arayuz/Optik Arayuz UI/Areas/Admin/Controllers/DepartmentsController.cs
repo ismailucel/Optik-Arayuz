5a134512f27e1fbc92b3055e69d60a954eb718c3
İsmail Üçel <ismailucel@gmail.com>
2023-05-31 16:43:08 +0300

    department controllers and views added

---
+++ Optik Arayuz/Optik Arayuz UI/Areas/Admin/Controllers/DepartmentsController.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Optik_Arayuz_UI.Data;
+using Optik_Arayuz_UI.Models;
+
+namespace Optik_Arayuz_UI.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class DepartmentsController : Controller
+    {
+        private readonly OptikArayuzDbContext _context;
+
+        public DepartmentsController(OptikArayuzDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Admin/Departments
+        public async Task<IActionResult> Index()
+        {
+            var optikArayuzDbContext = _context.Departments.Include(d => d.Faculty);
+            return View(await optikArayuzDbContext.ToListAsync());
+        }
+
+        // GET: Admin/Departments/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Departments == null)
+            {
+                return NotFound();
+            }
+
+            var department = await _context.Departments
+                .Include(d => d.Faculty)
+                .FirstOrDefaultAsync(m => m.DepartmentId == id);
+            if (department == null)
+            {
+                return NotFound();
+            }
+
+            return View(department);
+        }
+
+        // GET: Admin/Departments/Create
+        public IActionResult Create()
+        {
+            ViewData["FacultyId"] = new SelectList(_context.Faculties, "FacultyId", "FacultyId");
+            return View();
+        }
+
+        // POST: Admin/Departments/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("DepartmentId,DepartmentName,DepartmentAddress,DepartmentMail,DepartmentPhoneNumber,FacultyId")] Department department)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(department);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["FacultyId"] = new SelectList(_context.Faculties, "FacultyId", "FacultyId", department.FacultyId);
+            return View(department);
+        }
+
+        // GET: Admin/Departments/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Departments == null)
+            {
+                return NotFound();
+            }
+
+            var department = await _context.Departments.FindAsync(id);
+            if (department == null)
+            {
+                return NotFound();
+            }
+            ViewData["FacultyId"] = new SelectList(_context.Faculties, "FacultyId", "FacultyId", department.FacultyId);
+            return View(department);
+        }
+
+        // POST: Admin/Departments/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("DepartmentId,DepartmentName,DepartmentAddress,DepartmentMail,DepartmentPhoneNumber,FacultyId")] Department department)
+        {
+            if (id != department.DepartmentId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(department);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!DepartmentExists(department.DepartmentId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["FacultyId"] = new SelectList(_context.Faculties, "FacultyId", "FacultyId", department.FacultyId);
+            return View(department);
+        }
+
+        // GET: Admin/Departments/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Departments == null)
+            {
+                return NotFound();
+            }
+
+            var department = await _context.Departments
+                .Include(d => d.Faculty)
+                .FirstOrDefaultAsync(m => m.DepartmentId == id);
+            if (department == null)
+            {
+                return NotFound();
+            }
+
+            return View(department);
+        }
+
+        // POST: Admin/Departments/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Departments == null)
+            {
+                return Problem("Entity set 'OptikArayuzDbContext.Departments'  is null.");
+            }
+            var department = await _context.Departments.FindAsync(id);
+            if (department != null)
+            {
+                _context.Departments.Remove(department);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool DepartmentExists(int id)
+        {
+          return (_context.Departments?.Any(e => e.DepartmentId == id)).GetValueOrDefault();
+        }
+    }
+}
