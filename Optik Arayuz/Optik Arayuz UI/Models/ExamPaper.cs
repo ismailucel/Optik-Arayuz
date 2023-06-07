4c017fdbda1f214177ca6b49a65e31e699af622c
İsmail Üçel <ismailucel@gmail.com>
2023-06-07 09:01:10 +0300

    views changed

--- Optik Arayuz/Optik Arayuz UI/Models/ExamPaper.cs
+++ Optik Arayuz/Optik Arayuz UI/Models/ExamPaper.cs
@@ -6,7 +6,10 @@ namespace Optik_Arayuz_UI.Models
     {
         [Key]
         public int ExamPaperId { get; set; }
+
+        [Display(Name = "Sınav Kağıdı Adı")]
         public string? ExamPaperName { get; set; }
+        [Display(Name = "Sınav Kağıdı Başlık")]
         public string? ExamPaperTitle { get; set; }
     }
 }
